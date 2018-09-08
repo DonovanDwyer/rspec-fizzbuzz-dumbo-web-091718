a1ce045656e0613306238f2841e4dcfd78c3bc3b
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-08 15:04:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,5 +4,9 @@ def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
   elsif num % 5 == 0
-    return ""
+    return "Buzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  else
+    return nil
 end
